b1ae59f72dc31996d85c58f94e8acc4565aa731b
Yogesh Agrawal <yagrawal@componence.in>
2014-09-10 19:56:39 +0530

    changes.

--- run.js
+++ run.js
@@ -1,8 +1,8 @@
 var si = require('se-interpreter');
 var tr = new si.TestRun({"steps": [{"type":"get", "url":"http://www.google.com"}]}, "Go to Google");
-tr.wd.maximize(null, function(){
 tr.listener = si.getInterpreterListener(tr);
-tr.start();
+tr.start(function(){
+	tr.wd.maximize();
+});
 tr.next();
 tr.end();
-});
